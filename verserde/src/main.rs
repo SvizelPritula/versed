733644b5f8552a692f5a46dc5138a72cd1df9dad
Benjamin Swart <Benjaminswart@email.cz>
2025-08-10 17:46:39 +0200

    Extract error report generation into dedicated function

--- verserde/src/main.rs
+++ verserde/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{Read, Result, stdin};
 
-use ariadne::{Color, Label, Report, ReportKind, Source};
-use chumsky::Parser;
+use ariadne::{Color, Label, Report, ReportKind, sources};
+use chumsky::{Parser, error::Rich};
 
 use crate::syntax::lexer::lexer;
 
@@ -12,23 +12,30 @@ pub mod r#macro;
 pub mod metadata;
 pub mod syntax;
 
+fn make_report<'a, 'b>(
+    error: Rich<'a, char>,
+    filename: &'b str,
+) -> Report<'a, (&'b str, std::ops::Range<usize>)> {
+    Report::build(ReportKind::Error, (filename, error.span().into_range()))
+        .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+        .with_message(error.to_string())
+        .with_label(
+            Label::new((filename, error.span().into_range()))
+                .with_message(error.to_string())
+                .with_color(Color::Red),
+        )
+        .finish()
+}
+
 fn main() -> Result<()> {
     let mut src = String::new();
     stdin().lock().read_to_string(&mut src)?;
 
     let (tokens, errs) = lexer().parse(&src).into_output_errors();
 
+    let filename = "input";
     for error in errs {
-        Report::build(ReportKind::Error, error.span().into_range())
-            .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
-            .with_message(error.to_string())
-            .with_label(
-                Label::new(error.span().into_range())
-                    .with_message(error.reason().to_string())
-                    .with_color(Color::Red),
-            )
-            .finish()
-            .print(Source::from(&src))?
+        make_report(error, filename).print(sources([(filename, &src)]))?
     }
 
     if let Some(tokens) = tokens {
